960317816557a62545ecab2a959918003bef9617
James Wood <woodjames275@gmail.com>
2020-02-02 22:09:07 -0600

    add docstrings for click, finish spec for launch command

--- jetpack/cli.py
+++ jetpack/cli.py
@@ -7,6 +7,7 @@ import click
 
 # Standard libraries
 import sys
+import os
 
 # Our modules
 from .commands import launch
@@ -14,10 +15,13 @@ from .commands import launch
 # this is the base group for our utility, all commands will be attached to @cli
 @click.group()
 def cli():
-    pass
+    '''Jetpack, a utility for getting started quicker.'''
+    os.environ['JETFILE_PATH'] = '/Users/james/Documents/james/jetpack/jetpack/jetfile'
 
-# jetpack launch -p project_name 
+#jetpack launch -p project_name [-n]
 @cli.command('launch') 
-@click.option('-p', required = True, help = 'project_name')
-def run_launch(p):
-    launch.main(project_name = p)  # offload complex logic to module    
+@click.option('-n', is_flag=True, help = 'Skip search and create a new project in current directory.')
+@click.argument('project')
+def run_launch(n, project):
+    '''Finds or creates new project and launches VSCode editor'''
+    launch.main(new=n, project=project)  # offload complex logic to module 
