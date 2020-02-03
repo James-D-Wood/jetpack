b8d458fc0e3d67d7cdff75e2a0e8402813064e43
James Wood <woodjames275@gmail.com>
2020-02-02 22:08:39 -0600

    finalize launch command

--- jetpack/commands/launch.py
+++ jetpack/commands/launch.py
@@ -1,4 +1,45 @@
 import os
+import re
+from ..jetfile import Jetfile
+from ..exceptions import InputNotWhitelisted
 
-def main(project_name):
-    print("Creating... {}".format(project_name))
+
+def main(new, project):
+    
+    try:
+        jetfile = Jetfile()
+        if not re.match('\w', project):
+            # whitelist param name to disallow any unexpected errors
+            raise InputNotWhitelisted("Project names can only contain alphanumeric characters and the underscore character.")
+        if new:
+            try:
+                os.mkdir(project)
+                cwd = os.path.abspath(os.getcwd())
+                project_path = cwd + "/" + project
+                jetfile.add(project_path)
+                os.system("code {}".format(project))
+            except FileExistsError:
+                print("A project with the name {} already exists in this directory".format(project))
+            except OSError as e:
+                if str(e)[:10] == "[Errno 30]":
+                    print("Writing is not allowed in this directory. Failed to create project.")
+                else:
+                    print(e)
+        else:
+            result = jetfile.find(project)
+            if result:
+                os.system("code {}".format(result))
+            else:
+                print("Entry not in jetfile")
+    except InputNotWhitelisted as e:
+        print(e)
+
+
+       
+    # else
+        # search for project
+        # if found
+            # open code editor
+        # else
+            # display error message
+    
